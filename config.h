77065aab2c41ae7fadc65d1fbf38076dd1b4a09d
Maximilian Strele <SMproductive@tutanota.de>
2023-01-22 18:18:07 +0100

    bug fix

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ struct rule rules[] = {
 	/* sequence     event        lines */
 	{"\ek",   SCROLL_UP,   1},       /* [Alt] + k ( "\e" = [ESC] = "\033" = "^[" ) */
 	{"\ej",   SCROLL_DOWN, 1},       /* [Alt] + j ( "\e" = [ESC] = "\033" = "^[" ) */
-	{"\eK",   SCROLL_UP,   -1},       /* [Alt] + K
-	{"\eJ",   SCROLL_DOWN, -1},       /* [Alt] + J
+	{"\eK",   SCROLL_UP,   -1},       /* [Alt] + K */
+	{"\eJ",   SCROLL_DOWN, -1},       /* [Alt] + J */
 	{"\031",  SCROLL_UP,    1},       /* mouse wheel up ( "^Y" = "\031" ) */
 	{"\005",  SCROLL_DOWN,  1},       /* mouse wheel Down ( "^E" = "\005" ) */
 };
